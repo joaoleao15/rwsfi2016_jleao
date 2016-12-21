39defcc73227fbf1b139996f0d371e59d8f96996
joaoleao <joao.leao.jr@gmail.com>
2016-12-21 12:59:29 +0000

    New Version

--- player_jleao/src/player_jleao.cpp
+++ player_jleao/src/player_jleao.cpp
@@ -18,7 +18,7 @@ using namespace ros;
  */
 class MyPlayer: public rwsfi2016_libs::Player
 {
-  public:
+public:
 
     /**
      * @brief Constructor, nothing to be done here
@@ -29,11 +29,36 @@ class MyPlayer: public rwsfi2016_libs::Player
 
     void play(const rwsfi2016_msgs::MakeAPlay& msg)
     {
-      //Custom play behaviour. Now I will win the game
+        //Custom play behaviour. Now I will win the game
+        /*float ang_min = 9999;
+        int player_num = 0;
 
-      //Behaviour follow the closest prey
-      //move(msg.max_displacement, getAngleToPLayer(preys_team->players[0]));
-        move(msg.max_displacement, M_PI/30);
+        for(int i = 0; i < preys_team->players.size(); i++)
+        {
+                ang_min = getAngleToPLayer(preys_team->players[i]);
+                player_num = i;
+        }
+        move(msg.max_displacement, getAngleToPLayer(preys_team->players[player_num]));*/
+        double near_player_distance = 1000.0;
+                int index_near_player = 0;
+
+                std::cout << "Number of Preys: "<< preys_team->players.size() << std::endl;
+
+                for(int i=0; i < preys_team->players.size(); i++)
+                {
+                   // std::cout << preys_team->players[i] << std::endl;
+                    float distance = getDistanceToPlayer(preys_team->players[i]);
+                    //std::cout << "Get Distance: "<< isnan(distance) << std::endl;
+
+                    if( (distance < near_player_distance) && (!isnan(distance)))
+                    {
+                        near_player_distance = distance;
+                        index_near_player    = i;
+                    }
+                }
+
+              //Behaviour follow the closest prey
+              move(msg.max_displacement, getAngleToPLayer(preys_team->players[index_near_player]) );
     }
 };
 
@@ -46,18 +71,18 @@ class MyPlayer: public rwsfi2016_libs::Player
  */
 int main(int argc, char** argv)
 {
-  // ------------------------
-  //Replace this with your name
-  // ------------------------
-  string my_name = "jleao";
-  string my_pet = "/dog";
+    // ------------------------
+    //Replace this with your name
+    // ------------------------
+    string my_name = "jleao";
+    string my_pet = "/dog";
 
-  //initialize ROS stuff
-  ros::init(argc, argv, my_name);
+    //initialize ROS stuff
+    ros::init(argc, argv, my_name);
 
-  //Creating an instance of class MyPlayer
-  MyPlayer my_player(my_name, my_pet);
+    //Creating an instance of class MyPlayer
+    MyPlayer my_player(my_name, my_pet);
 
-  //Infinite spinning (until ctrl-c)
-  ros::spin();
+    //Infinite spinning (until ctrl-c)
+    ros::spin();
 }
